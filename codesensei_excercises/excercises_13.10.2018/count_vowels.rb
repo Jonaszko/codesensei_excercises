a079578cee48c15d1f3be84035a80a9bf5727b36
jonaszko <konieczny.jonasz@gmail.com>
2018-10-16 11:01:36 +0200

    count vowels

---
+++ codesensei_excercises/excercises_13.10.2018/count_vowels.rb
@@ -0,0 +1,15 @@
+# The function counts the vowel in the string given as an argument.
+
+# @param [String] a single string.
+# @return [Integer] number of vowels in the string given as an argument.
+def count_vowel(words)
+  vowels_counter = 0
+  vowels = ['a', 'e', 'o', 'u', 'i', 'y']
+  tab_of_letters = words.split('')
+  vowels.each do |a|
+    tab_of_letters.each do |b|
+      if a == b; vowels_counter += 1; end
+    end
+  end
+  print("number of vowels in this string is #{vowels_counter}")
+end
