947d4e1db2845be3a53688997abf265ae62c729e
jonaszko <konieczny.jonasz@gmail.com>
2018-11-04 15:38:20 +0100

    rock paper scissors improve 1

--- codesensei_excercises/excercises_13.10.2018/rps_game.rb
+++ codesensei_excercises/excercises_13.10.2018/rps_game.rb
@@ -32,29 +32,28 @@ def rps_game(player1, player2)
   end
 end
 
+# This is a function that simulates the game "rock paper scissors".
+# This function takes two string arguments and then determines the winner
+# based on their values. The number of the argument determines the player
+# number.
+
+# @param [String], [String]
+# @return [String]
 def rps_game_2(player1, player2)
-  legend = "One of the players has input a wrong argument." +
-  "The correct options to enter are: \"rock\" or \"paper\" " +
-  "or \"scissors\"."
-  options = ["rock", "paper", "scissors"]
-  if (player1 == options[1]) && (player2 == options[0])
-    return "player 1 win"
-  elsif (player1 == options[1]) && (player2 == "scissors")
-    return "player 2 win"
-  elsif (player1 == options[1]) && (player2 == options[1])
-    return "a draw was obtained"
-  elsif (player1 == "rock") && (player2 == "rock")
-    return "a draw was obtained"
-  elsif (player1 == "rock") && (player2 == "scissors")
-    return "player 1 win"
-  elsif (player1 == "rock") && (player2 == options[1])
-    return "player 2 win"
-  elsif (player1 == "scissors") && (player2 == "rock")
-    return "player 2 win"
-  elsif (player1 == "scissors") && (player2 == "scissors")
-    return "a draw was obtained"
-  elsif (player1 == "scissors") && (player2 == options[1])
-    return "player 1 win"
+  legend = 'One of the players has input a wrong argument.' +
+  'The correct options to enter are: rock or paper ' +
+  'or scissors.'
+  options = ['rock', 'paper', 'scissors']
+  if ((player1 == options[1]) && (player2 == options[0])) ||
+  ((player1 == options[0]) && (player2 == options[2])) ||
+  ((player1 == options[2]) && (player2 == options[1]))
+    return 'player 1 win'
+  elsif ((player1 == options[1]) && (player2 == options[2])) ||
+  ((player1 == options[0]) && (player2 == options[1])) ||
+  ((player1 == options[2]) && (player2 == options[0]))
+    return 'player 2 win'
+  elsif options.include?(player1) && (player1 == player2)
+    return 'a draw was obtained'
   else
     return legend
   end
